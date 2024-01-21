41028bf98d82d603856bf459bb9faaddcfe1ffbd
Yadav-Vishnu <yadav143vishnu@gmail.com>
2024-01-21 20:15:58 +0530

    Added the data edit functionality to edit button

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CategoriesComponent } from './categories/categories.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:DashboardComponent
+    },
+    {
+        path:'categories',
+        component:CategoriesComponent
+    }
+];
